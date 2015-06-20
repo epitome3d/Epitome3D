50d9f796529ccfdbd13063bde2e41941b203ff67
Arthur Pachachura <pachachura.arthur@gmail.com>
2015-06-20 17:03:22 -0500

    Update libs AND FIX EVERYTHING!!!#@@!!!

--- epitome-core/src/epitome-core.h
+++ epitome-core/src/epitome-core.h
@@ -1,5 +1,13 @@
 #pragma once
 
+#ifdef _WINDOWS
+#define WINDOWS
+#endif
+
+#ifdef __gnu_linux__
+#define LINUX
+#endif
+
 #include "EPITOME-CORE/init.cpp" //functions to initialize Epitome3D
 #include "EPITOME-CORE/error.h" //initializes error functions
 #include "EPITOME-CORE/system.h" //initializes system specifications
--- epitome-tester/main.cpp
+++ epitome-tester/main.cpp
@@ -1,15 +1,17 @@
-#include <epitome-core.h>
+//#include <epitome-core.h>
 #include <iostream>
-using namespace EPITOME;
+//using namespace EPITOME;
 
-#include <GL/freeglut.h>
-//#pragma comment(lib, "freeglut.lib")
-//#pragma comment(lib, "freeglutd.lib")
+//#include <GL/freeglut.h>
+//#include <GL/glu.h>
+#define GLFW_INCLUDE_GLU
+#include <GL/glut.h>
+#include <GLFW/glfw3.h>
 
 #define window_width 640
 #define window_height 480
 
-// Initialize OpenGL perspective matrix
+//Initialize OpenGL perspective matrix
 void Setup(int width, int height)
 {
 	glViewport(0, 0, width, height);
@@ -19,64 +21,103 @@ void Setup(int width, int height)
 	glMatrixMode(GL_MODELVIEW);
 }
 
-void Render()
+static void Key(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-	// Z angle
-	static float angle;
-	// Clear color (screen)
-	// And depth (used internally to block obstructed objects)
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	// Load identity matrix
-	glLoadIdentity();
-	// Multiply in translation matrix
-	glTranslatef(0, 0, -10);
-	// Multiply in rotation matrix
-	glRotatef(angle, 0, 0, 1);
-	// Render colored quad
-	glBegin(GL_QUADS);
-	glColor3ub(255, 000, 000); glVertex2f(-1, 1);
-	glColor3ub(000, 255, 000); glVertex2f(1, 1);
-	glColor3ub(000, 000, 255); glVertex2f(1, -1);
-	glColor3ub(255, 255, 000); glVertex2f(-1, -1);
-	glEnd();
-	// Swap buffers (color buffers, makes previous render visible)
-	glutSwapBuffers();
-	// Increase angle to rotate
-	angle += 0.25;
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
 }
 
-void Blank() { }
-
-void Init()
+static void Error(int error, const char* description)
 {
-	glutInitWindowSize(window_width, window_height);
-	glutInitWindowPosition(100, 100);
-	glutCreateWindow("Tutorial 01");
-	glutIdleFunc(Render);
-	glutDisplayFunc(Blank);
-	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-	Setup(window_width, window_height);
+	fputs(description, stderr);
 }
 
-#ifdef WINDOWS
+GLFWwindow* Init()
+{
+	//set error function
+	glfwSetErrorCallback(Error);
+
+	//create window
+	GLFWwindow* window = glfwCreateWindow(window_width, window_height, "Tutorial 01", NULL, NULL);
+	if (!window) {
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+	glfwMakeContextCurrent(window);
+
+	//set key callbacks
+	glfwSetKeyCallback(window, Key);
+
+	//get window size
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+
+	Setup(width, height);
+
+	return window;
+}
+
+void Loop(GLFWwindow* window)
+{
+	while (!glfwWindowShouldClose(window))
+	{
+		//keep running
+		// Z angle
+		static float angle;
+		// Clear color (screen)
+		// And depth (used internally to block obstructed objects)
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		// Load identity matrix
+		glLoadIdentity();
+		// Multiply in translation matrix
+		glTranslatef(0, 0, -10);
+		// Multiply in rotation matrix
+		glRotatef(angle, 0, 0, 1);
+		// Render colored quad
+		glBegin(GL_QUADS);
+		glColor3ub(255, 000, 000); glVertex2f(-1, 1);
+		glColor3ub(000, 255, 000); glVertex2f(1, 1);
+		glColor3ub(000, 000, 255); glVertex2f(1, -1);
+		glColor3ub(255, 255, 000); glVertex2f(-1, -1);
+		glEnd();
+		// Swap buffers (color buffers, makes previous render visible)
+		glfwSwapBuffers(window);
+		// Increase angle to rotate
+		angle += 0.25;
+
+		//checks for events
+		glfwPollEvents();
+	}
+}
+
+#ifdef _WINDOWS
+#include <windows.h>
+#include <shellapi.h>
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdshow)
 {
-	EPITOME::Initialize();
-	Init();
-	glutMainLoop();
-	EPITOME::Exit();
+	LPWSTR *szArglist;
+	int argc;
+	char** argv;
+	szArglist = CommandLineToArgvW(GetCommandLineW(), &argc);
+	argv = (char**)szArglist;
 
+	if (!glfwInit())
+		exit(EXIT_FAILURE);
+	GLFWwindow* window = Init();
+	Loop(window);
+	glfwTerminate();
 	return 0;
 }
-#endif
 
-#ifdef LINUX
+#else
+
 int main(int argc, char** argv)
 {
-	EPITOME::Initialize(argc, argv);
-	Init();
-	glutMainLoop();
-
+	if (!glfwInit())
+		exit(EXIT_FAILURE);
+	GLFWwindow* window = Init();
+	Loop(window);
+	glfwTerminate();
 	return 0;
 }
 #endif
